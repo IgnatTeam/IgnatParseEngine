42da6e238a28b067954b2338f1384f46db0ab6ed
Юрий <juriy.d359@outlook.com>
2019-06-15 06:56:45 +0300

    Добавьте файлы проекта.

---
+++ Parser.cs
@@ -0,0 +1,60 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgnatParseEngine
+{
+    public static class Parser
+    {
+        public static string GetPageSource(string url)
+        {
+            var req = WebRequest.Create(url);
+            var res = req.GetResponse();
+            string result;
+            using (var sr = new StreamReader(res.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+            res.Close();
+            return result;
+        }
+
+        public static void SavePageSource(string url, string path)
+        {
+            var req = WebRequest.Create(url);
+            var res = req.GetResponse();
+            using (var sr = new StreamReader(res.GetResponseStream()))
+            {
+                using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+                {
+                    sw.Write(sr.ReadToEnd());
+                    sw.Close();
+                }
+                sr.Close();
+            }
+            res.Close();
+        }
+
+        public static IHtmlDocument GetPageDocument(string url)
+        {
+            var req = WebRequest.Create(url);
+            var res = req.GetResponse();
+            string result;
+            using (var sr = new StreamReader(res.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+            res.Close();
+            var parser = new HtmlParser();
+            return parser.ParseDocument(result);
+        }
+    }
+}
---
+++ StringEx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IgnatParseEngine
+{
+    public static class StringEx
+    {
+        // text,tag => <tag>text</tag>
+        public static string ToTag(this string s, string tag)
+        {
+            return $"<{tag}>{s}</{tag}>";
+        }
+
+        public static string JoinAll(this string[] arr, string separator)
+        {
+            return string.Join(separator, arr);
+        }
+
+        public static string ReFind(this string s, string pattern)
+        {
+            var regex = new Regex(pattern);
+            var match = regex.Match(s);
+            if (match.Success)
+            {
+                return match.Value;
+            }
+            return null;
+        }
+
+        public static string ReFindGroup(this string s, string pattern, string gpName)
+        {
+            var regex = new Regex(pattern);
+            var match = regex.Match(s);
+            if (match.Groups[gpName].Success)
+            {
+                return match.Groups[gpName].Value;
+            }
+            return null;
+        }
+
+        public static List<string> ReFindAll(this string s, string pattern)
+        {
+            var regex = new Regex(pattern);
+            var match = regex.Match(s);
+            var list = new List<string>();
+            if (match.Success)
+            {
+                list.Add(match.Value);
+                match = match.NextMatch();
+            }
+            return list;
+        }
+
+        public static string ReRemove(this string s, string pattern)
+        {
+            var regex = new Regex(pattern);
+            return regex.Replace(s, "");
+        }
+
+        public static string ReReplace(this string s, string pattern, string replacement)
+        {
+            var regex = new Regex(pattern);
+            return regex.Replace(s, replacement);
+        }
+    }
+}
